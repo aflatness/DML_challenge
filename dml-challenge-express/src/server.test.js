98a11b8a8cedc92b14d01059a9791e7da6bebad5
aflatness <aflatness@gmail.com>
2021-04-19 13:49:02 -0600

    Tests written for creating and saving new users

--- dml-challenge-express/src/server.test.js
+++ dml-challenge-express/src/server.test.js
@@ -13,16 +13,47 @@ describe('Server endpoints', () => {
   })
 
   describe('Getting all surveys', () => {
-    it('should return a 200 status code', async (done) => {
+    it('should return a 200 status code', async () => {
       const res = await request.get('/surveys')
       expect(res.status).toBe(200)
-      done()
     })
 
-    it('should return an array', async (done) => {
+    it('should return an array', async () => {
       const res = await request.get('/surveys')
       expect(res.body).toHaveLength(0)
-      done()
     })
+  })
+
+  describe('Creating users/surveys', () => {
+    const data = {
+      first: 'Joe',
+      last: 'Schmoe',
+      email: 'Joe@schmoe.com'
+    }
+    it('should return a 200 status code for valid creation', async () => {
+      const res = await request.post('/surveys').send(data)
+      expect(res.status).toBe(201)
+    })
+
+    it('should return the newly saved user', async () => {
+      const res = await request.post('/surveys').send(data)
+      expect(res.body).not.toBeNull()
+      expect(res.body).toHaveProperty('first')
+      expect(res.body.first).toBe(data.first)
+      expect(res.body.response).toBeNull()
+      expect(res.body.uuid).toHaveLength(36)
+    })
+
+    describe('retrieval of users after creation', () => {
+      it('should return all the users that have been added', async () => {
+        const res = await request.get('/surveys')
+        console.log(res.body)
+        expect(res.body).toHaveLength(2)
+      })
+    })
+  })
+
+  describe('Patch request for saving a users survey response', () => {
+
   })
 })
